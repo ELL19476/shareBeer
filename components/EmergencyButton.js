00d0346145de318c2a63e75b31b2dae035b09a5b
ELL19476 <ell19476@spengergasse.at>
2021-03-27 20:52:03 +0100

    Svg fix

--- components/EmergencyButton.js
+++ components/EmergencyButton.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import { StyleSheet, Image, Text } from 'react-native';
-import Logo from '~/assets/Logo'
+import { StyleSheet, Text, Image } from 'react-native';
+import Logo from '~/assets/logo.svg'
 
 class EmergencyButton extends React.Component {
     render() {
         return (
-            <Logo />
+           <Logo />
         );
     }
 }
